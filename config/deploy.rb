45673e3f6e936be7dfc9fe765465e275b6216b00
Laurent <lau.cazanove@gmail.com>
2017-03-01 17:02:44 +0100

    Edited repository url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.2"
 
 set :application, "twitter_on_rails"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:Strift/twitterOnRails.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
