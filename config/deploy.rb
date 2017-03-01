23ff1506de35b819831cd6fd1eabd0181f19bb58
Laurent <lau.cazanove@gmail.com>
2017-03-01 18:44:42 +0100

    Added unicorn pids folder to linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,10 +22,9 @@ set :repo_url, "git@github.com:Strift/twitterOnRails.git"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_files, "pids"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
