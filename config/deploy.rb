df19cfb7a032d33f9a71e9ad3ae162ae0e9fd8f0
Laurent <lau.cazanove@gmail.com>
2017-03-02 12:09:59 +0100

    Fixed path error

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:Strift/twitterOnRails.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/Strift/twitter_on_rails"
+set :deploy_to, "/home/Strift/twitter_on_rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
